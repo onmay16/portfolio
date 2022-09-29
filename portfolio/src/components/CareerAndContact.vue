fff121b99cfe6b6633637b769b44785c5bd2a905
Sugyeong <onmay16@gmail.com>
2022-09-28 21:18:50 -0500

    combine career page & contact page

---
+++ portfolio/src/components/CareerAndContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="career-n-contact flex-row">
+        <div class="career flex-column">
+            <h1 class="career-text">Career</h1>
+            <div class="career-info flex-row">
+                <img src="../assets/icons/lumere-logo.svg" alt="Lumere Logo" class="lumere-logo">
+                <div class="vl"></div>
+                <div class="career-info">
+                    <div class="career-company">GHX - Lumere</div>
+                    <div class="career-position">Software Engineer I</div>
+                    <div class="career-period">2022.09~</div>
+                </div>
+            </div>
+        </div>
+        <div class="contact">
+            <h1 class="contact-text">Contact</h1>
+            <div class="contact-content">If you want a very quick conversation about ANYTHING, please leave me a message through LinkedIn! Email is also fine if you prefer that. All the contacts availabe are under ‘About me’. If you want to check out my pixel art, click here! :)</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import '../static/style.css';
+@import './CareerAndContact.css';
+</style>
